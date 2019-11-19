a2b2d9bab7629f187b7a024ee6bea27e0973cc31
quentin <user.name>
2019-11-19 02:13:15 +0100

    add coap

---
+++ README.md
@@ -0,0 +1,9 @@
+# raspberry-mosquitto
+
+this project is meant to discover the MQTT protocol and the CoAP protocol.
+
+## Installation
+
+Python 3.5 required minimum
+
+`pip install paho-mqtt aiocoap`
---
+++ coap_client.py
@@ -0,0 +1,37 @@
+import logging
+
+from asyncio import *
+from aiocoap import *
+
+logging.basicConfig(level=logging.INFO)
+
+async def get():
+    protocol = await Context.create_client_context()
+
+    request = Message(code=GET, uri="coap://localhost/time")
+
+    try:
+        response = await protocol.request(request).response
+
+    except Exception as e:
+        print('Failed to fetch resource')
+        print(e)
+    else:
+        print(f"Result: {response.code} \n {response.payload}")
+
+
+async def put():
+    context = await Context.create_client_context()
+
+    await sleep(2)
+
+    payload = b"I hope this will not break the internet..."
+    request = Message(code=PUT, payload=payload, uri="coap://localhost/other/block")
+
+    response = await context.request(request).response
+
+    print(f"Result: {response.code} \n {response.payload}")
+
+if __name__ == "__main__":
+    get_event_loop().run_until_complete(get())
+    get_event_loop().run_until_complete(put())
